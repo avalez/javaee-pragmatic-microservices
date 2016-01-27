5aee5e10e9d1594d32e6a411a8913b2a5168bb53
Andriy Zhdanov <azhdanov@gmail.com>
2016-01-27 17:40:18 +0100

    create domain offline no to require server restart

--- wlst/deploy.py
+++ wlst/deploy.py
@@ -1,4 +1,4 @@
-connect('admin', 'admin123', 't3://localhost:7001')
+connect('weblogic', 'welcome1', 't3://localhost:7001')
 
-deploy(appName='cargo-tracker', path='../cargo-tracker/target/cargo-tracker.war', resourceGroup='ResourceGroup-0', partition='Partition-0')
-deploy(appName='path-finder', path='../path-finder/target/path-finder.war', resourceGroup='ResourceGroup-0', partition='Partition-1')
+deploy(appName='cargo-tracker', path='cargo-tracker/target/cargo-tracker.war', resourceGroup='ResourceGroup-0', partition='Partition-0')
+deploy(appName='path-finder', path='path-finder/target/path-finder.war', resourceGroup='ResourceGroup-0', partition='Partition-1')
--- wlst/start-partitions.py
+++ wlst/start-partitions.py
@@ -1,4 +1,4 @@
-connect("admin", "admin123", "t3://localhost:7001")
+connect("weblogic", "welcome1", "t3://localhost:7001")
 
 domainRuntime()
 cd('/')

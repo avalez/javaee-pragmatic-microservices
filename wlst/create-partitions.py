81484f40b490a84b29d431440045ac63a59b7de4
Andriy Zhdanov <azhdanov@gmail.com>
2016-02-02 15:13:39 +0100

    cleanup

--- wlst/create-partitions.py
+++ wlst/create-partitions.py
@@ -3,6 +3,7 @@ connect('weblogic', 'welcome1', 't3://localhost:7001')
 edit()
 startEdit()
 
+# Create Domain Partiton for Cargo Tracker
 cd('/')
 cmo.createPartition('Partition-0')
 cd('/Partitions/Partition-0')
@@ -14,6 +15,7 @@ cmo.setUseDefaultTarget(true)
 cmo.setTargets(None)
 cmo.findEffectiveTargets()
 
+# Create Domain Partiton for Path Finder
 cd('/')
 cmo.createPartition('Partition-1')
 cd('/Partitions/Partition-1')
@@ -27,3 +29,11 @@ cmo.findEffectiveTargets()
 
 save()
 activate()
+
+# Start both partitions
+domainRuntime()
+cd('/')
+taskMbean1 = cmo.lookupDomainPartitionRuntime('Partition-0').getPartitionLifeCycleRuntime().start()
+taskMbean2 = cmo.lookupDomainPartitionRuntime('Partition-1').getPartitionLifeCycleRuntime().start()
+while (taskMbean1.getStatus() != 'TASK COMPLETED' and taskMbean2.getStatus() != 'TASK COMPLETED'):import time;time.sleep(1)
+
--- wlst/start-partitions.py
+++
@@ -1,7 +0,0 @@
-connect("weblogic", "welcome1", "t3://localhost:7001")
-
-domainRuntime()
-cd('/')
-taskMbean1 = cmo.lookupDomainPartitionRuntime('Partition-0').getPartitionLifeCycleRuntime().start()
-taskMbean2 = cmo.lookupDomainPartitionRuntime('Partition-1').getPartitionLifeCycleRuntime().start()
-while (taskMbean1.getStatus() != 'TASK COMPLETED' and taskMbean2.getStatus() != 'TASK COMPLETED'):import time;time.sleep(1)
